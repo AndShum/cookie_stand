e637296c703447693321dac8e72cc12356895a8d
AndShum <ashumway2@gmail.com>
2016-09-26 16:30:59 -0700

    the arrays are working, not showing on page

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var firstAndPikeEl = document.getElementById('firstAndPikeShop');
 var hour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var firstAndPikeInfo = {
   minCustomer: 23,
@@ -11,25 +10,35 @@ var firstAndPikeInfo = {
     for(var i = 0; i < hour.length; i++){
       min = Math.ceil(this.minCustomer);
       max = Math.floor(this.maxCustomer);
-      this.randCustPerHour.push(Math.floor(Math.random() * (max - min)) + min);
+      this.randCustPerHour.push(Math.ceil(Math.random() * (max - min)) + min);
     }
   },
-  dailyAvgSales: [],
-  calcAvgSales: function generateAvgSales(){
+  dailySales: 0,
+  hourlySales: [],
+  calcSales: function(){
     for (var i = 0; i < hour.length; i++) {
-      var liFirstAndPike = document.createElement('li');
-      var firstAndPikeAvg = 6.3;
-      console.log(hour[i] + ' ' + this.randCustPerHour + ' customers');
-      liFirstAndPike.textContent = Math.floor(this.randCustPerHour() / firstAndPikeAvg);
-      firstAndPikeEl.appendChild(liFirstAndPike);
+      this.hourlySales.push(Math.ceil(this.randCustPerHour[i] * this.avgSale));
     }
+
   }
 
 };
 
-firstAndPikeInfo.calcAvgSales(liFirstAndPike.textContent);
-console.log(firstAndPikeInfo.randCustPerHour);
+console.log(firstAndPikeInfo);
+firstAndPikeInfo.calcCustPerHour();
+firstAndPikeInfo.calcSales();
+console.log(firstAndPikeInfo);
 
+// var firstAndPikeEl = document.getElementById('firstAndPikeShop');
+// var liFirstAndPike = document.createElement('li');
+// liFirstAndPike.textContent = Math.floor(firstAndPikeInfo.randCustPerHour * firstAndPikeAvg);
+// firstAndPikeEl.appendChild(liFirstAndPike);
+//
+//
+//
+// firstAndPikeInfo.calcAvgSales(this.liFirstAndPike);
+// console.log(firstAndPikeInfo.randCustPerHour);
 
+//
 // firstAndPikeInfo.calcCustPerHour();
 // console.log(firstAndPikeInfo.randCustPerHour);
