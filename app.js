a039a6a6657511eb4bafc47c816f0d832141873a
AndShum <ashumway2@gmail.com>
2016-09-28 10:25:19 -0700

    comments added

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-//object for first and pike location
 
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-var allStores = [];
-function StoreInfo(locationName, minCustomer, maxCustomer, avgSale){
+var allStores = []; //stores the arrays for each of the stores
+function StoreInfo(locationName, minCustomer, maxCustomer, avgSale){ //constructor creating the array's for the stores and rendering the table data
   this.locationName = locationName;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
@@ -12,14 +11,14 @@ function StoreInfo(locationName, minCustomer, maxCustomer, avgSale){
   this.dailySales = 0;
   this.randCustPerHour = [];
   this.hourlySales = [];
-  this.calcCustPerHour = function randomCustNum(min, max){
+  this.calcCustPerHour = function randomCustNum(min, max){ //calulates random # of customer for each hour between the min & max numbers provided
     for(var i = 0; i < hours.length; i++){
       min = Math.ceil(this.minCustomer);
       max = Math.floor(this.maxCustomer);
       this.randCustPerHour.push(Math.ceil(Math.random() * (max - min)) + min);
     }
   };
-  this.calcSales = function(){
+  this.calcSales = function(){ //calculates the # of cookies sold each hour
     for (var i = 0; i < hours.length; i++) {
       this.hourlySales.push(Math.ceil(this.randCustPerHour[i] * this.avgSale));
       this.dailySales += this.hourlySales[i];
@@ -28,25 +27,23 @@ function StoreInfo(locationName, minCustomer, maxCustomer, avgSale){
 
   this.calcCustPerHour();
   this.calcSales();
-  allStores.push(this);
+  allStores.push(this); //pushes data from calc functions to allStores array
 
-
-  console.log(allStores);
-  this.render = function(){              //creates and renders the li elements for the seatac ul
-    var salesTable = document.getElementById('salesFigures');
-    var tableRow = document.createElement('tr');
-    var storeName = document.createElement('td');
-    storeName.textContent = this.locationName;
-    tableRow.appendChild(storeName);
-    for (var i = 0; i < hours.length; i++){
-      var tableData = document.createElement('td');
-      tableData.textContent = this.hourlySales[i];
-      tableRow.appendChild(tableData);
+  this.render = function(){ //renders all of the data into the table element in the sales.html
+    var salesTable = document.getElementById('salesFigures');//connects this function with table element in sales.html
+    var tableRow = document.createElement('tr');//creates a table row element to be filled
+    var storeName = document.createElement('td');//creates a table cell that will be placed into the table row
+    storeName.textContent = this.locationName;//creates the text that will be contained within the teble cell
+    tableRow.appendChild(storeName);//appends the created table cell into the table row
+    for (var i = 0; i < hours.length; i++){ //loop that will create the cells to hold the hourly cookie sales for each store
+      var tableData = document.createElement('td'); //creates new table cell
+      tableData.textContent = this.hourlySales[i]; //creates the text to be held in the table cell
+      tableRow.appendChild(tableData); //appends the new cell to the table row
     }
-    var saleTotals = document.createElement('td');
-    saleTotals.textContent = this.dailySales;
-    tableRow.appendChild(saleTotals);
-    salesTable.appendChild(tableRow);
+    var saleTotals = document.createElement('td'); //creates a new table cell
+    saleTotals.textContent = this.dailySales; //creates text to be in the table cell for the total daily sales
+    tableRow.appendChild(saleTotals); //appends table cell to the table row
+    salesTable.appendChild(tableRow); //appends the now completed table row to the table element in sales.html
   };
 }
 
@@ -99,3 +96,5 @@ function createTableFooter(){
   salesTable.appendChild(tableRow);
 }
 createTableFooter();
+ //document.createAttribute
+ //att.value = this.name
